404f1b67c8009b56c91b6013db551a8ee93c5a32
lbjay <lbjay@reallywow.com>
2014-04-01 13:48:36 -0400

    added statsd & graphite config

--- puppet/manifests/stage2.pp
+++ puppet/manifests/stage2.pp
@@ -1,20 +1,23 @@
 #Set global path for exec calls
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin" ] }
 
+class { 'graphite': }
+
+class { 'statsd':
+  graphiteserver   => 'localhost',
+  flushinterval    => 1000, # flush every second
+  percentthreshold => [75, 90, 99],
+  address          => '10.20.1.2',
+  listenport       => 2158,
+  provider         => npm,
+  require => [Class[graphite]],
+}
+
 # starts redis on 127.0.0.1:6379
 class { 'redis':
   version => '2.6.5',
   redis_port => '6378',
   redis_bind_address => '0.0.0.0' 
-}
-
-class { 'logstash':
-  manage_repo => true,
-  repo_version => '1.4',
-  restart_on_change => false,
-  status => 'running',
-  init_defaults_file => '/vagrant/logstash/logstash_defaults',
-  require   => [Class[redis]]
 }
 
 class { 'elasticsearch':
@@ -47,6 +50,15 @@ elasticsearch::plugin {'elasticsearch/marvel/latest':
   module_dir => 'marvel'
 }
 
+class { 'logstash':
+  manage_repo => true,
+  repo_version => '1.4',
+  restart_on_change => false,
+  status => 'running',
+  init_defaults_file => '/vagrant/logstash/logstash_defaults',
+  require   => [Class[statsd], Class[redis], Class[elasticsearch]]
+}
+
 logstash::configfile { 'configname':
    source => '/vagrant/logstash/logstash.conf',
 }
@@ -56,6 +68,3 @@ file { '/etc/logstash/patterns/ads-patterns':
   source => '/vagrant/logstash/patterns/ads-patterns',
   require => [Class[logstash]]
 }
-
-# "This installs Kibana and starts it running on port 5601 and connects to an Elasticsearch index running on localhost:9200"
-#include 'kibana'
