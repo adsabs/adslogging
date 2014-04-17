ac75cd56827af29fd86512f20175d291ecb32823
Jay Luker <jluker@cfa.harvard.edu>
2014-04-17 09:45:28 -0400

    * logstash.conf rewrite w/ conditionals * fabfile reworking to make build/run of individual containers simpler

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
 import os
+import sys
 from os.path import abspath, dirname
 
 from fabric.api import task, local, env
-from fabric.context_managers import settings, cd
+from fabric.context_managers import settings, cd, hide
 from fabric.decorators import with_settings
 
 env.base_dir = abspath(dirname(__file__))
@@ -20,48 +21,79 @@ config = {
     }
 }
 
-def docker(cmd, sudo=False):
+def create_task(name, conf):
+    def _task():
+        env.containers = [name]
+    t = task(name=name)(_task)
+    setattr(sys.modules[__name__], name, t)
+    return t
+    
+# generate tasks for each defined container
+for container, conf in config.iteritems():
+    create_task(container, conf)
+    
+@task
+def all():
+    env.containers = config.keys()
+
+def docker(cmd, sudo=False, **kwargs):
     with cd(env.base_dir):
         sudo = sudo and "sudo" or ""
-        return local("%s docker %s" % (sudo,cmd))
+        return local("%s docker %s" % (sudo,cmd), **kwargs)
+
+def sudo_docker(cmd, **kwargs):
+    return docker(cmd, True, **kwargs)
     
-env.run = docker
+env.docker = docker
 
 @task
 def sudo():
-    env.run = lambda x: docker(x, True)
+    env.docker = sudo_docker
 
 @task
 @with_settings(warn_only=True)
-def build(image=None, rmi=False):
-    for name in config.iterkeys():
-        if image is not None and image != name:
-            continue
-        if rmi:
-            env.run("rmi adsabs/%s" % name)
-        env.run("build -t adsabs/%s dockerfiles/%s" % (name, name))
+def build():
+    for name in env.containers:
+        env.docker("build -t adsabs/%s dockerfiles/%s" % (name, name))
         
 @task
 @with_settings(warn_only=True)
-def run(container=None, rm=False, ep=''):
+def rmi():
+    for name in env.containers:
+        env.docker("rmi adsabs/%s" % name)
+        
+@task
+@with_settings(warn_only=True)
+def run(ep=''):
+    for name in env.containers:
+        conf = config[name]
+        ports = conf.has_key('ports') and ' '.join("-p %s" % p for p in conf['ports']) or ''
+        vfrom = conf.has_key('vfrom') and '--volumes-from %s' % conf['vfrom'] or ''
+        entrypoint = conf.has_key('entrypoint') and conf['entrypoint'] or ep
+        env.docker("run -d -t -i --name adsabs-%s %s %s adsabs/%s %s" % (name, ports, vfrom, name, entrypoint))
     
+@task
+@with_settings(warn_only=True)
+def data():
     # start the data container
-    if rm:
-        env.run("stop adsabs-adsloggingdata")
-        env.run("rm adsabs-adsloggingdata")
-    env.run('run --name="adsabs-adsloggingdata" -v /data -v /var/log/supervisor ventz/dataos true')
+    with settings(hide('running', 'stdout', 'stderr')):
+        containers = env.docker('ps -a', capture=True)
+        if 'adsabs-adsloggingdata' in containers:
+            env.docker("stop adsabs-adsloggingdata")
+            env.docker("rm adsabs-adsloggingdata")
+            env.docker('run --name="adsabs-adsloggingdata" -v /data -v /var/log/supervisor ventz/dataos true')
 
-    for name, conf in config.iteritems():
-        if container is not None and container != name:
-            continue
-        if rm:
-            env.run("stop adsabs-%s" % name)
-            env.run("rm adsabs-%s" % name)
-        else:
-            ports = conf.has_key('ports') and ' '.join("-p %s" % p for p in conf['ports']) or ''
-            vfrom = conf.has_key('vfrom') and '--volumes-from %s' % conf['vfrom'] or ''
-            entrypoint = conf.has_key('entrypoint') and conf['entrypoint'] or ep
-            env.run("run -d -t -i --name adsabs-%s %s %s adsabs/%s %s" % (name, ports, vfrom, name, entrypoint))
+@task
+@with_settings(warn_only=True)
+def stop():
+    for c in env.containers:
+        env.docker("stop adsabs-%s" % c)
+    
+@task
+@with_settings(warn_only=True)
+def rm():
+    for c in env.containers:
+        env.docker("rm adsabs-%s" % c)
             
             
             
