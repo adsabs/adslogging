8ecfb241af256876c595abc498afd51c37adc1c3
Jay Luker <jluker@cfa.harvard.edu>
2014-04-24 14:16:47 -0400

    manage redis config so we can specify storage dir

--- dockerfiles/logstash/assets/run.sh
+++ dockerfiles/logstash/assets/run.sh
@@ -6,6 +6,7 @@ mkdir -p /usr/share/elasticsearch/data
 chown -R elasticsearch /usr/share/elasticsearch/data
 mkdir -p /data/elasticsearch
 chown -R elasticsearch /data/elasticsearch
+mkdir -p /data/redis
 
 sed -i -e "s/statsd { host => \"localhost\"/statsd { host => \"$statsd_host\" sender => \"$HOSTNAME\"/g" /etc/logstash/logstash.conf
 
