47ddf056b18cda156faa1c8de15679d9083bf7ca
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-07-01 11:00:30 -0400

    Fix usermod command

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get install -y python-pip python-dev curl
 curl -s https://get.docker.io/ubuntu/ | sudo sh
 pip install fabric
-sudo usermod -a G docker vagrant
+sudo usermod -a -G docker vagrant
